cb15ffcfbb7d811ef17cb1cc4aeb31eaf0453a58
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-06-22 13:03:19 +0200

    Prepared stm created.

--- src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
+++ src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
@@ -7,6 +7,32 @@ public class JdbcMain {
     public static void main(String[] args) {
 
         statement();
+        System.out.println("************************");
+        preparedStatement();
+
+
+
+    }
+
+    private static void preparedStatement() {
+        int minSalary = 100;
+        try (Connection connection = getConnection()) {
+            String query =
+                    "select ename, job, sal " +
+                            "from sdajdbc.employee " +
+                            "where sal > ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,minSalary);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                System.out.println(ename + " " + job + " " + sal);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void statement() {
@@ -26,8 +52,6 @@ public class JdbcMain {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
