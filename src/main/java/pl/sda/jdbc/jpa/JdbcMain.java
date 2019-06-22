cf2b78f15a1b0c2271bb18d6ff40be9376250d25
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-06-22 12:37:08 +0200

    First sql query created.

--- src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
+++ src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
@@ -1,20 +1,44 @@
 package pl.sda.jdbc.jpa;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcMain {
 
     public static void main(String[] args) {
 
+        statement();
     }
 
-    private static Connection getConnection(){
-        try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306");
+    private static void statement() {
+        try (Connection connection = getConnection()) {
+            String query =
+                    "select ename, job, sal " +
+                            "from sdajdbc.employee " +
+                            "where sal > 100";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                System.out.println(ename + " " + job + " " + sal);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+
+    }
+
+
+    private static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306?useUnicode=true&serverTimezone=UTC",
+                    "root", "1910");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
