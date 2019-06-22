7cf98ba5a82a39663a141629eafeda658dc71d94
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-06-22 13:34:38 +0200

    Prepared stm - null value secured created.

--- src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
+++ src/main/java/pl/sda/jdbc/jpa/JdbcMain.java
@@ -9,22 +9,49 @@ public class JdbcMain {
         statement();
         System.out.println("************************");
         preparedStatement();
+        System.out.println("************************");
         callableStatement();
-
+        System.out.println("************************");
+        preparedStatement2();
 
 
     }
 
+    private static void preparedStatement2() {
+        int minSalary = 100;
+        try (Connection connection = getConnection()) {
+            String query =
+                    "select ename, job, sal, mgr " +
+                            "from sdajdbc.employee " +
+                            "where sal > ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, minSalary);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                Integer mgr = resultSet.getInt("mgr");
+                if (resultSet.wasNull()) {
+                    mgr = null;
+                }
+                System.out.println(ename + " " + job + " " + sal + " " + mgr);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void callableStatement() {
-        try(Connection connection = getConnection();){
+        try (Connection connection = getConnection();) {
             String query = "{call sdajdbc.getName(?,?)}";
             CallableStatement callableStatement = connection.prepareCall(query);
-            callableStatement.setInt(1,7654);
-            callableStatement.registerOutParameter(2,Types.VARCHAR);
+            callableStatement.setInt(1, 7654);
+            callableStatement.registerOutParameter(2, Types.VARCHAR);
             callableStatement.execute();
             String string = callableStatement.getString(2);
             System.out.println(string);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
@@ -38,9 +65,9 @@ public class JdbcMain {
                             "from sdajdbc.employee " +
                             "where sal > ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,minSalary);
+            preparedStatement.setInt(1, minSalary);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String ename = resultSet.getString("ename");
                 String job = resultSet.getString("job");
                 int sal = resultSet.getInt("sal");
